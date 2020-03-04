88f1e01cbe30366056af891f7af5c955f9dffd6e
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2020-03-04 13:09:29 -0600

    new material

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
+// Dotenv
 const PORT = process.env.PORT || 5000;
 
 app.listen(
