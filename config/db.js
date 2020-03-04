b1fbb68fecda85755e368649d57fd2726ed9102a
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2020-03-04 04:06:53 -0600

    enviroment var

--- config/db.js
+++ config/db.js
@@ -2,14 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      process.env.MONGO_URI || "mongodb://localhost/expensetracker",
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true
-      }
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
 
     console.log(
       `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
